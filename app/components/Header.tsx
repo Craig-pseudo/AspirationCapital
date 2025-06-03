1ce461d08d73b1b7ea01a43a638ddc5a4bccc4f8
Craig Pseudo <craigdp15@gmail.com>
2025-06-03 15:29:13 +0200

    Header

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -2,8 +2,17 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <div className='bg-blue-200 flex justify-between'>
+        <div className='flex justify-between items-center'>
+            <div>
+                <img src="/images/AC_logo.jpg" alt="AC Logo" className="w-32 h-auto" />
+            </div>
+            <div>Aspiration Capital</div>
+        </div>
+        <div className='h-8 w-25 rounded-md m-2 flex justify-center items-center bg-amber-200'>Contact Us</div>
+    </div>
   )
 }
 
 export default Header
+
