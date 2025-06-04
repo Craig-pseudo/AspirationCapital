b81ea63500e8fcb7922c5983f2ce76814801ccbb
Craig Pseudo <craigdp15@gmail.com>
2025-06-04 22:32:29 +0200

    Size Change

--- app/components/WhyChooseUS.tsx
+++ app/components/WhyChooseUS.tsx
@@ -4,7 +4,7 @@ const WhyChooseUs = () => {
   return (
     <section
       id="why-choose-us"
-      className="bg-[#333333] text-[#C8C8C8] border-b-2 border-[#C8C8C8] py-16 px-6 md:px-12 font-sans max-w-7xl mx-auto"
+      className="bg-[#333333] text-[#C8C8C8] border-b-2 border-[#C8C8C8] py-8 px-6 md:px-6 font-sans max-w-7xl mx-auto"
     >
       <h2 className="text-3xl font-medium mb-8 tracking-wide drop-shadow-sm">
         Why Choose Us
