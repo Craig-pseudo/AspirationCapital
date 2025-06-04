8c9499ee53df8dc21d418fb5df37f21cf1e880d4
Craig Pseudo <craigdp15@gmail.com>
2025-06-04 17:13:11 +0200

    Header Details

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -2,14 +2,35 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <div className='bg-[#1D2D44] flex justify-between'>
+    <div className='bg-[#333333] flex justify-between items-center w-full border-b-2 border-[#C8C8C8]'>
         <div className='flex justify-between items-center'>
             <div>
                 <img src="/images/AC_Logo.png" alt="AC Logo" className="w-32 h-auto" />
             </div>
-            <div className='text-[#C0C0C0] flex justify-center'>Aspiration Capital</div>
+            <div className='text-[#C8C8C8] flex justify-center text-3xl'>Aspiration Capital</div>
         </div>
-        <div className='h-8 w-25 rounded-md m-2 flex justify-center items-center bg-amber-200'>Contact Us</div>
+        <div>
+          
+          <nav className="flex space-x-6 text-[#C8C8C8] font-medium">
+      <a href="#about" className="hover:text-[#E0E0E0] transition-colors duration-200">
+        About
+      </a>
+      <a href="#investment-focus" className="hover:text-[#E0E0E0] transition-colors duration-200">
+        Focus
+      </a>
+      <a href="#portfolio" className="hover:text-[#E0E0E0] transition-colors duration-200">
+        Portfolio
+      </a>
+      <a href="#team" className="hover:text-[#E0E0E0] transition-colors duration-200">
+        Team
+      </a>
+      <a href="#why-choose-us" className="hover:text-[#E0E0E0] transition-colors duration-200">
+        Why Choose Us
+      </a>
+      </nav>
+
+        </div>
+        <div className='h-8 w-25 rounded-md m-2 flex justify-center items-center text-[#333333] bg-[#C8C8C8]'>Contact Us</div>
     </div>
   )
 }
