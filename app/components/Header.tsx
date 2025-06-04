5ffd23174ec7482cc8f9cd66ec19731da8c35959
Craig Pseudo <craigdp15@gmail.com>
2025-06-04 10:31:15 +0200

    Header Colours

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -2,12 +2,12 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <div className='bg-blue-200 flex justify-between'>
+    <div className='bg-[#1D2D44] flex justify-between'>
         <div className='flex justify-between items-center'>
             <div>
-                <img src="/images/AC_logo.jpg" alt="AC Logo" className="w-32 h-auto" />
+                <img src="/images/AC_Logo.png" alt="AC Logo" className="w-32 h-auto" />
             </div>
-            <div className='flex justify-center'>Aspiration Capital</div>
+            <div className='text-[#C0C0C0] flex justify-center'>Aspiration Capital</div>
         </div>
         <div className='h-8 w-25 rounded-md m-2 flex justify-center items-center bg-amber-200'>Contact Us</div>
     </div>
